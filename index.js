cef81159939c156c509516ef31e77e2a46a7adae
Dalenacio <dalenacio@gmail.com>
2025-05-14 15:25:54 +0100

    Add pre-ready loading
    Necessary for website hosts like render to avoid port scan while loading data

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 import express from "express";
-import { structureData } from "./structurer.js";
-import { getCache } from './cache.js';
-import './poller.js';
-import { updateInfo } from "./updater.js";
+import { initDatabase, getGeneralData, getRouteData } from "./database.js";
 
 const port = 3000;
 const app = express();
@@ -11,19 +8,35 @@ let isReady = false;
 app.use(express.static("public"))
 
 app.get('/health', (req, res) => {
-  res.status(isReady ? 200 : 503).json({ 
-    ready: isReady,
-    message: isReady ? "OK" : "Initializing data..." 
-  });
+  if (isReady) {
+    res.status(200).json({ 
+      ready: true,
+      message: "OK" 
+    });
+  } else {
+    res.status(503).json({ 
+      ready: false,
+      message: "Server is initializing, database setup in progress. Please wait." 
+    });
+  }
 });
+
+async function initializeData() {
+  console.log("Database initialization process started...");
+  try {
+    await initDatabase();
+    isReady = true;
+    console.log("Database initialization complete. Server is now fully ready.");
+  } catch (error) {
+    console.error("Error during database initialization:", error);
+  }
+}
 
 async function startServer() {
   try {
-    await updateInfo();
-    isReady = true;
-    
     app.listen(port, () => {
       console.log(`Server running on port ${port}`);
+      initializeData()
     });
   } catch (error) {
     console.error("Failed to initialize:", error);
@@ -31,13 +44,20 @@ async function startServer() {
   }
 }
 
-app.get("/", async (req, res) =>{
-    const data = await structureData()
+app.get("/", async (req, res) => {
+  if (!isReady) {
+    res.status(503).render("loading.ejs");
+    return;
+  }
+    const data = await getGeneralData()
     res.render("index.ejs", {data: data})
 })
 
 app.get("/route/:routeId", async (req, res) => {
-    const data = await structureData()
+    if (!isReady) {
+      res.status(503).render("loading", { title: "Initializing - Please Wait" });
+      return;
+    }
     const chosenRoute = req.params.routeId;
     res.render("routeInfo.ejs", {routeId : chosenRoute, data: data})
 });
